448b0b253d601f64861f5f7d1172576c1cb148d7
TorreP303 <TorreP303@TorreP303-PC7>
2016-03-16 19:29:51 -0500

    Cambios de Jamer y Erika

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -15,7 +15,7 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola, CompuMundo HyperMegaRed");
+        System.out.println("Hola, CompuMundo HyperMegaRed, soy Erika sin tilde");
         int a,b;
         a=7;
         b=8;
